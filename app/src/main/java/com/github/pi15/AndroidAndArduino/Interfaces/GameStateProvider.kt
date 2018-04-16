504510bb090cb56108636e5df4096d120e6334d8
Yuriy <natarur@yandex.com>
2018-04-17 00:10:13 +0300

    start&stop in the GameStateProvider

--- app/src/main/java/com/github/pi15/AndroidAndArduino/Interfaces/GameStateProvider.kt
+++ app/src/main/java/com/github/pi15/AndroidAndArduino/Interfaces/GameStateProvider.kt
@@ -4,4 +4,14 @@ import com.github.pi15.AndroidAndArduino.Containers.GameState
 
 interface GameStateProvider {
     val gameState : GameState
+
+    /**
+     * Starts the provider
+     */
+    fun start()
+
+    /**
+     * Stops the provider
+     */
+    fun stop()
 }
