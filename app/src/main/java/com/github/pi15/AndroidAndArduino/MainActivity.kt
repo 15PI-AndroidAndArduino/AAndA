45912946dcd719cf06c25c44a04ea3637c7175e2
Yuriy <natarur@yandex.com>
2018-04-16 20:02:14 +0300

    Empty project

---
+++ app/src/main/java/com/github/pi15/AndroidAndArduino/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.pi15.AndroidAndArduino
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
