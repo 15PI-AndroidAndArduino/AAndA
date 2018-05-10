97872df67f066a559c08681c5c183f99603d59d8
Yuriy <natarur@yandex.com>
2018-05-11 01:37:20 +0300

    mp3 + ui integ wip

--- app/src/main/java/com/github/pi15/AndroidAndArduino/Activities/MainActivity.kt
+++ app/src/main/java/com/github/pi15/AndroidAndArduino/Activities/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.github.pi15.AndroidAndArduino.Activities
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import com.github.pi15.AndroidAndArduino.Frontend.GameView
 import android.util.DisplayMetrics
-import android.view.WindowManager
-import com.github.pi15.AndroidAndArduino.Frontend.DummyGsp
 import com.github.pi15.AndroidAndArduino.Interfaces.GameStateProvider
+import com.github.pi15.AndroidAndArduino.Providers.GameEngine
+import com.github.pi15.AndroidAndArduino.R
 
 
 class MainActivity : Activity() {
@@ -28,7 +27,9 @@ class MainActivity : Activity() {
 
         val dpHeight = getDpHeight()
 
-        gsProvider = DummyGsp()//TODO("GS")
+        val stream = resources.openRawResource(R.raw.km)
+
+        gsProvider = GameEngine(stream, 4.0,15.0)
         gsProvider.start()
         setContentView(GameView(this, gsProvider, 200))
     }
